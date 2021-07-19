70fc80c506add327805e522fd9bf1b64c1be2dc6
Rozon <rozon617@gmail.com>
2021-07-19 19:44:29 +0800

    009-引入公共组件：顶部通栏、头部和底部

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,20 +1,26 @@
 <template>
-  <nav>公共顶部通栏</nav>
-  <header>公共头部</header>
+  <!-- 顶部通栏布局 -->
+  <AppTopnav></AppTopnav>
+  <!-- 公共头部 -->
+  <AppHeader></AppHeader>
   <div>
-      main 主体 - 可以替换为首页，分类页，详情页
-      <!-- 二级路由显示区域 -->
-      <router-view></router-view>
+    main 主体 - 可以替换为首页，分类页，详情页
+    <!-- 二级路由显示区域 -->
+    <router-view></router-view>
   </div>
-  <footer></footer>
+  <!-- 公共底部 -->
+  <AppFooter></AppFooter>
 </template>
 
 <script>
+import AppHeader from '@/components/app-header.vue'
+import AppTopnav from '@/components/app-topnav.vue'
+import AppFooter from '@/components/app-footer.vue'
 export default {
+  components: { AppHeader, AppTopnav, AppFooter },
   name: 'xtx-layout'
 }
 </script>
 
 <style>
-
 </style>
