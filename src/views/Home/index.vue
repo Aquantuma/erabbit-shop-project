b1b4e64635b4e3daceae4762b0de9e9583dbcac7
Rozon <rozon617@gmail.com>
2021-07-22 10:19:14 +0800

    024-首页主体-轮播图-基础布局

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,16 +4,19 @@
       <div class="container">
         <!-- 左侧分类 -->
         <HomeCategory></HomeCategory>
+        <!-- 轮播图 -->
+        <HomeBanner></HomeBanner>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import HomeBanner from './components/home-banner.vue'
 import HomeCategory from './components/home-category.vue'
 export default {
   name: 'PageHome',
-  components: { HomeCategory }
+  components: { HomeCategory, HomeBanner }
 }
 </script>
 
